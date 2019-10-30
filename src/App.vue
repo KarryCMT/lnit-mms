50fee9c1a8da48d1febf99c7f9003da4c56ea37a
KarryCMT <1570606465@qq.com>
2019-10-30 16:19:37 +0800

    修改提交

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@
   </div>
 </template>
 <style >
+body,
+html,
+#app{
+    width: 100%;
+    height: 100%;
+}
   body{
-    font-family: "微软雅黑"
+    font-family: "微软雅黑";
+    margin: 0;
+    padding: 0;
   }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,99 +1,127 @@
 <template>
-   <div class="login-container">
-        <el-form :model="ruleForm2" :rules="rules2"
-         status-icon
-         ref="ruleForm2" 
-         label-position="left" 
-         label-width="0px" 
-         class="demo-ruleForm login-page">
-            <h3 class="title">冷暖IT会员管理系统</h3>
-            <el-form-item prop="username">
-                <el-input type="text" 
-                    v-model="ruleForm2.username" 
-                    auto-complete="off" 
-                    placeholder="用户名"
-                ></el-input>
-            </el-form-item>
-                <el-form-item prop="password">
-                    <el-input type="password" 
-                        v-model="ruleForm2.password" 
-                        auto-complete="off" 
-                        placeholder="密码"
-                    ></el-input>
-                </el-form-item>
-            <el-checkbox 
-                v-model="checked"
-                class="rememberme"
-            >记住密码</el-checkbox>
-            <el-form-item style="width:100%;">
-                <el-button type="primary" style="width:100%;" @click="handleSubmit" :loading="logining">登录</el-button>
-            </el-form-item>
-        </el-form>
+  <div id="Login">
+    <div class="login-container">
+      <el-form
+        :model="form"
+        :rules="rules"
+        status-icon
+        label-position="left"
+        label-width="0px"
+        ref="form"
+        class="demo-ruleForm login-page"
+      >
+        <h3 class="title">冷暖IT会员管理系统</h3>
+        <el-form-item prop="username">
+          <el-input type="text" v-model="form.username" auto-complete="off" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="form.password"
+            auto-complete="off"
+            placeholder="密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item style="width:100%;">
+          <el-button type="primary" style="width:100%;" @click="submitForm('form')" >登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
+import {login,getUserInfo} from '@/api/login'
 export default {
-  data(){
-        return {
-            logining: false,
-            ruleForm2: {
-                username: 'admin',
-                password: '123456',
-            },
-            rules2: {
-                username: [{required: true, message: 'please enter your account', trigger: 'blur'}],
-                password: [{required: true, message: 'enter your password', trigger: 'blur'}]
-            },
-            checked: false
-        }
-    },
-    methods: {
-        handleSubmit(event){
-            this.$refs.ruleForm2.validate((valid) => {
-                if(valid){
-                    this.logining = true;
-                    if(this.ruleForm2.username === 'admin' && 
-                       this.ruleForm2.password === '123456'){
-                           this.logining = false;
-                           sessionStorage.setItem('user', this.ruleForm2.username);
-                           this.$router.push({path: '/'});
-                    }else{
-                        this.logining = false;
-                        this.$alert('username or password wrong!', 'info', {
-                            confirmButtonText: 'ok'
-                        })
-                    }
-                }else{
-                    console.log('error submit!');
-                    return false;
-                }
-            })
-        }
+  data() {
+    return {
+      form: {
+          username:'',
+          password:''
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入账号", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" }
+        ]
+      },
     }
-};
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+            //提交表单给后台进行验证是否正确
+            login(this.form.username,this.form.password).then(response => {
+                const resp = response.data
+                console.log(resp,resp.flag)
+                if(resp.flag){
+                    //验证成功，通过token去获取用户信息
+                    getUserInfo(resp.data.token)
+                    const respUser = response.data
+                    if(respUser.flag){
+                        //获取到了用户的数据
+                        console.log('userInfo',respUser.data)
+                        // 1.保存token，用户信息
+                        localStorage.setItem('lengnuanit-msm-user',JSON.stringify(respUser.data))
+                        localStorage.setItem('lengnuanit-msm-token',resp.data.token)
+                        // 2.前往首页
+                        this.$router.push('/')
+                    }else{
+                        // 未通过,弹出警告
+                        this.$message({
+                        message: respUser.message,
+                        type: 'warning'
+                    });
+                    }
+                } 
+            })
+          }else{
+           
+            // alert(resp.message)
+             
+          } 
+        })
+        } 
+      }
+   }
 </script>
 <style scoped>
+#Login {
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/background.jpg");
+  background-size: cover;
+}
 .login-container {
-    width: 100%;
-    height: 100%;
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 .login-page {
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    margin: 180px auto;
-    width: 350px;
-    padding: 35px 35px 15px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
+  position: absolute;
+  right: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  width: 350px;
+  height: 310px;
+  padding: 35px 35px 15px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
 }
 label.el-checkbox.rememberme {
-    margin: 0px 0px 15px;
-    text-align: left;
+  margin: 0px 0px 15px;
+  text-align: left;
 }
-.title{
-    text-align: center;
+.title {
+  text-align: center;
 }
 </style>
     
